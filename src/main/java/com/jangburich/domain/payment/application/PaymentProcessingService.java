6dde9645285cf01688df43e36e5b0bbe1625c5cb
HyunWoo9930 <ohw9930@hufs.ac.kr>
2024-11-18 23:27:07 +0900

    fix : KakaoPay 결제시 성공 로직 수정

--- src/main/java/com/jangburich/domain/payment/application/PaymentProcessingService.java
+++ src/main/java/com/jangburich/domain/payment/application/PaymentProcessingService.java
@@ -1,25 +1,27 @@
 package com.jangburich.domain.payment.application;
 
+import org.springframework.stereotype.Service;
+
 import com.jangburich.domain.payment.application.strategy.PaymentServiceStrategy;
 import com.jangburich.domain.payment.dto.request.PayRequest;
 import com.jangburich.domain.payment.dto.response.ApproveResponse;
 import com.jangburich.domain.payment.dto.response.ReadyResponse;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentProcessingService {
 
-    private final PaymentServiceStrategy paymentServiceStrategy;
+	private final PaymentServiceStrategy paymentServiceStrategy;
 
-    public ReadyResponse processPayment(Long userId, PayRequest payRequest) {
-        PaymentService paymentService = paymentServiceStrategy.getPaymentService(payRequest.paymentType());
-        return paymentService.payReady(userId, payRequest);
-    }
+	public ReadyResponse processPayment(Long userId, PayRequest payRequest) {
+		PaymentService paymentService = paymentServiceStrategy.getPaymentService(payRequest.paymentType());
+		return paymentService.payReady(userId, payRequest);
+	}
 
-    public ApproveResponse processSuccess(Long userId, String tid, String pgToken) {
-        PaymentService paymentService = paymentServiceStrategy.getPaymentService("kakao");
-        return paymentService.payApprove(userId, tid, pgToken);
-    }
+	public ApproveResponse processSuccess(String pgToken) {
+		PaymentService paymentService = paymentServiceStrategy.getPaymentService("kakao");
+		return paymentService.payApprove(pgToken);
+	}
 }
