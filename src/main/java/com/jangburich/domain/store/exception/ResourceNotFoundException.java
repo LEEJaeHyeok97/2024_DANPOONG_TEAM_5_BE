7b3d80e34cc307b1f6832b48640cf3ee20e2934b
LEEJaeHyeok97 <hazardous10@naver.com>
2024-11-19 03:14:19 +0900

    feat: 카테고리 별 가게 목록 조회 구현

---
+++ src/main/java/com/jangburich/domain/store/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.jangburich.domain.store.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
