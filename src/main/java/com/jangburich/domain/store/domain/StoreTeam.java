35f7b680785dd7ce619ca2954650cccfdc480ecf
LEEJaeHyeok97 <hazardous10@naver.com>
2024-11-19 00:08:43 +0900

    feat: storeTeam 테이블 생성

---
+++ src/main/java/com/jangburich/domain/store/domain/StoreTeam.java
@@ -0,0 +1,34 @@
+package com.jangburich.domain.store.domain;
+
+import com.jangburich.domain.common.BaseEntity;
+import com.jangburich.domain.team.domain.Team;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StoreTeam extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+}
