c6f9bd4491ca8ae57b002096a6bdf24fcd95c74f
이재혁 <hazardous10@naver.com>
2024-11-19 12:41:33 +0900

    feat: 장바구니 조회 기능 구현 (#43)
    * feat: 장바구니 추가 기능 구현
    
    * feat: 장바구니 조회 기능 구현

--- src/main/java/com/jangburich/domain/order/domain/repository/CartRepository.java
+++ src/main/java/com/jangburich/domain/order/domain/repository/CartRepository.java
@@ -1,6 +1,8 @@
 package com.jangburich.domain.order.domain.repository;
 
 import com.jangburich.domain.order.domain.Cart;
+import com.jangburich.domain.user.domain.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +13,6 @@ import org.springframework.stereotype.Repository;
 public interface CartRepository extends JpaRepository<Cart, Long> {
     @Query("SELECT c FROM Cart c WHERE c.user.userId = :userId AND c.menu.id = :menuId")
     Optional<Cart> findByUserIdAndMenuId(@Param("userId") Long userId, @Param("menuId") Long menuId);
+
+    List<Cart> findAllByUser(User user);
 }
