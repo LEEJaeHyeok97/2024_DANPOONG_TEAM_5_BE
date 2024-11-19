b5de2ba7d4aea49dd2f9d233a22bfdd01c974ab7
LEEJaeHyeok97 <hazardous10@naver.com>
2024-11-19 12:39:07 +0900

    feat: 장바구니 조회 기능 구현

--- src/main/java/com/jangburich/domain/order/presentation/OrderController.java
+++ src/main/java/com/jangburich/domain/order/presentation/OrderController.java
@@ -2,6 +2,7 @@ package com.jangburich.domain.order.presentation;
 
 import com.jangburich.domain.order.application.OrderService;
 import com.jangburich.domain.order.dto.request.AddCartRequest;
+import com.jangburich.domain.order.dto.response.CartResponse;
 import com.jangburich.global.payload.Message;
 import com.jangburich.global.payload.ResponseCustom;
 import com.jangburich.utils.parser.AuthenticationParser;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +25,19 @@ public class OrderController {
     private final OrderService orderService;
 
     @Operation(summary = "장바구니 담기", description = "장바구니에 물건과 수량을 담습니다.")
-    @PostMapping("/cart")
+    @PostMapping("/carts")
     public ResponseCustom<Message> addCart(
             Authentication authentication,
             @RequestBody AddCartRequest addCartRequest
     ) {
         return ResponseCustom.OK(orderService.addCart(AuthenticationParser.parseUserId(authentication), addCartRequest));
+    }
+
+    @Operation(summary = "장바구니 조회", description = "장바구니에 담은 상품을 조회합니다.")
+    @GetMapping("/carts")
+    public ResponseCustom<CartResponse> getCartItems(
+            Authentication authentication
+    ) {
+        return ResponseCustom.OK(orderService.getCartItems(AuthenticationParser.parseUserId(authentication)));
     }
 }
