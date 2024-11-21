4c73bf5416bf64f05448ab9c9691b9d3c323d687
LEEJaeHyeok97 <hazardous10@naver.com>
2024-11-21 13:01:48 +0900

    feat: 그룹 멤버 전체 조회 기능 구현

--- src/main/java/com/jangburich/domain/team/domain/repository/UserTeamRepository.java
+++ src/main/java/com/jangburich/domain/team/domain/repository/UserTeamRepository.java
@@ -1,5 +1,6 @@
 package com.jangburich.domain.team.domain.repository;
 
+import com.jangburich.domain.common.Status;
 import com.jangburich.domain.team.domain.Team;
 import com.jangburich.domain.team.domain.UserTeam;
 import com.jangburich.domain.user.domain.User;
@@ -13,4 +14,6 @@ public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
     boolean existsByUserAndTeam(User user, Team team);
 
     List<UserTeam> findAllByTeam(Team team);
+
+    List<UserTeam> findAllByTeamAndStatus(Team team, Status status);
 }
