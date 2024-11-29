4de12c8979c8fa164d359876a3e78fcc30950f20
HyunWoo9930 <ohw9930@hufs.ac.kr>
2024-11-29 16:56:35 +0900

    fix : 카카오페이 url 수정, category 수정

--- src/main/java/com/jangburich/domain/payment/dto/response/ReadyResponse.java
+++ src/main/java/com/jangburich/domain/payment/dto/response/ReadyResponse.java
@@ -4,6 +4,8 @@ public record ReadyResponse(
         String tid,
         String next_redirect_pc_url,
         String android_app_scheme,
-        String ios_app_scheme
+        String ios_app_scheme,
+		String next_redirect_app_url,
+		String next_redirect_mobile_url
 ) {
 }
