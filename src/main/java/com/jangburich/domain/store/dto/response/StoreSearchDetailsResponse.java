734e6e782f52006e5b953d2bfb35dd402c706b31
HyunWoo9930 <ohw9930@hufs.ac.kr>
2024-12-06 16:58:24 +0900

    fix : prepay 기능 수정, excel authentication 수정

--- src/main/java/com/jangburich/domain/store/dto/response/StoreSearchDetailsResponse.java
+++ src/main/java/com/jangburich/domain/store/dto/response/StoreSearchDetailsResponse.java
@@ -2,6 +2,9 @@ package com.jangburich.domain.store.dto.response;
 
 import com.jangburich.domain.store.domain.Category;
 import com.querydsl.core.annotations.QueryProjection;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public record StoreSearchDetailsResponse(
@@ -25,9 +28,18 @@ public record StoreSearchDetailsResponse(
         this.category = category;
         this.address = address;
         this.operationStatus = operationStatus;
-        this.closeTime = closeTime;
+        this.closeTime = formatCloseTime(closeTime);
         this.contactNumber = contactNumber;
         this.representativeImg = representativeImg;
         this.storeMenus = storeMenus;
+    }
+
+    private static String formatCloseTime(String closeTime) {
+        try {
+            LocalTime time = LocalTime.parse(closeTime.split("\\.")[0]);
+            return time.format(DateTimeFormatter.ofPattern("HH:mm"));
+        } catch (Exception e) {
+            return closeTime;
+        }
     }
 }
