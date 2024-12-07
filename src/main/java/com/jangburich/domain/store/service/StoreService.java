463eca21c65e0a41baf7becb07601cef01135b5f
HyunWoo9930 <ohw9930@hufs.ac.kr>
2024-12-07 20:32:33 +0900

    fix : 날짜 로직 변경

--- src/main/java/com/jangburich/domain/store/service/StoreService.java
+++ src/main/java/com/jangburich/domain/store/service/StoreService.java
@@ -299,8 +299,8 @@ public class StoreService {
 				for (Cart cart : cartRepository.findAllByOrders(order)) {
 					price += cart.getMenu().getPrice();
 				}
-				return new OrderResponse(order.getId(), order.getUser().getName(), order.getUpdatedAt(),
-					String.valueOf(price));
+				LocalDate date = order.getUpdatedAt().toLocalDate();
+				return new OrderResponse(order.getId(), order.getUser().getName(), date, String.valueOf(price));
 			})
 			.sorted(Comparator.comparing(OrderResponse::getDate).reversed())
 			.toList();
